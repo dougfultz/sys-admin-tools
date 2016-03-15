dbbd69f9fa2d9d35620d035a51ffcb7919046f3c
dougfultz <dougfultz@users.noreply.github.com>
2016-03-14 23:40:04 -0400

    check username

--- bin/rescan-all-disks.py
+++ bin/rescan-all-disks.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python
+#----------------------------------------------------------
+import subprocess
+#=Main=====================================================
+if __name__ == "__main__":
+
+    username = subprocess.check_output("whoami").strip()
+    if (username != "root"):
+        print("You must be root")
+        exit(1)
+
