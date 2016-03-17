ebe5e1147ab30a2e78b2cd77447e898d9ca515ba
dougfultz <dougfultz@users.noreply.github.com>
2016-03-17 11:42:19 -0400

    added functions

--- bin/rescan-all-disks.py
+++ bin/rescan-all-disks.py
@@ -1,6 +1,25 @@
 #!/usr/bin/python
 #----------------------------------------------------------
+import glob
 import subprocess
+#----------------------------------------------------------
+def scanScsi():
+    fileList = glob.glob("/sys/class/scsi_host/host*/scan")
+
+    for i in fileList:
+        with open(i,'a') as f:
+            f.write("- - -")
+#----------------------------------------------------------
+def scanBlock():
+    fileList = glob.glob("/sys/block/*/device/rescan")
+
+    for i in fileList:
+        with open(i,'a') as f:
+            f.write("1")
+#----------------------------------------------------------
+def debShowDisks():
+    devices = subprocess.Popen("lshw -short | grep \"dev\"",shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+    print(devices.stdout)
 #=Main=====================================================
 if __name__ == "__main__":
 
@@ -9,3 +28,11 @@ if __name__ == "__main__":
         print("You must be root")
         exit(1)
 
+    print("Scanning SCSI controller...")
+    scanScsi()
+
+    print("Scanning block devices...")
+    scanBlock
+
+    print("Showing disks:")
+    debShowDisks
