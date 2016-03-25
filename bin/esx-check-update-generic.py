9fc78cdde6e216465a695ab73c87facedd459565
dougfultz <dougfultz@users.noreply.github.com>
2016-03-25 10:38:22 -0400

    Enable firewall rule

--- bin/esx-check-update-generic.py
+++ bin/esx-check-update-generic.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 #----------------------------------------------------------
-
+import subprocess
+#----------------------------------------------------------
+username="root"
+sshkey="/home/username/.ssh/id-rsa"
+hostname="IP-ADDRESS"
+repo="https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml"
+PerformUpgrade=False
 #=Main=====================================================
 if __name__ == "__main__":
-    print("run")
+    #Enable firewall rule
+    subprocess.call(["ssh","-i",sshkey,"-l",username,hostname,"esxcli network firewall ruleset set -e true -r httpClient"])
