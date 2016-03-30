0441e33febe60579e1673e528a93aa813444f202
dougfultz <dougfultz@users.noreply.github.com>
2016-03-29 23:06:07 -0400

    Perform dry run and perform upgrade.

--- bin/esxi-update.py
+++ bin/esxi-update.py
@@ -21,6 +21,30 @@ if __name__ == "__main__":
     if (type(updateList) != type(str())):
         updateList=updateList.decode()
     if "update" in updateList.lower():
+
+        #Show available updates
         for i in updateList.splitlines():
             if "status" in i.lower() or "--" in i.lower() or "update" in i.lower():
                 print(i)
+
+        #Perform a dry run
+        subprocess.call(["ssh","-i",sshkey,"-l",username,hostname,"esxcli software vib update --dry-run -d "+repo])
+
+        if PerformUpgrade:
+            subprocess.call(["ssh","-i",sshkey,"-l",username,hostname,"esxcli software vib update -d "+repo])
+
+#Appendix
+#Installing patches on an ESXi 5.x/6.x host from the command line
+#http://kb.vmware.com/kb/2008939
+
+#About the ESXi 5.x and 6.0 firewall
+#http://kb.vmware.com/kb/2005284
+
+#Show current image profile
+# esxcli software profile get
+
+#Show available image profiles from depot
+# esxcli software sources profile list -d $REPO
+
+#Update profile
+# esxcli software profile update -p $PROFILE -d $REPO
