5d954a625e03d98f355e1cb3cd493507bbad4709
dougfultz <dougfultz@users.noreply.github.com>
2016-04-13 10:15:43 -0400

    converted to functions

--- bin/apt-mirror-status.sh
+++ bin/apt-mirror-status.sh
@@ -10,21 +10,28 @@
 showIndexLog=false
 showArchiveLog=false
 #----------------------------------------------------------
+function indexStatus() {
+    echo "Index status:"
+
+    for i in `ls /var/spool/apt-mirror/var/index-urls.* | cut -d. -f2`
+    do
+        TOTAL=`wc -l /var/spool/apt-mirror/var/index-urls.$i | awk '{print $1}'`
+        CURRENT=`grep -E "saved|not retrieving|404 Not Found" /var/spool/apt-mirror/var/index-log.$i | wc -l`
+        echo "    $i - $CURRENT/$TOTAL"
+    done
+}
+#----------------------------------------------------------
+function archiveStatus() {
+    echo "Archive status:"
+
+    for i in `ls /var/spool/apt-mirror/var/archive-urls.* | cut -d. -f2`
+    do
+        TOTAL=`wc -l /var/spool/apt-mirror/var/archive-urls.$i | awk '{print $1}'`
+        CURRENT=`grep saved /var/spool/apt-mirror/var/archive-log.$i | wc -l`
+        echo "    $i - $CURRENT/$TOTAL"
+    done
+}
 #=Main=====================================================
-echo "Index status:"
 
-for i in `ls /var/spool/apt-mirror/var/index-urls.* | cut -d. -f2`
-do
-    TOTAL=`wc -l /var/spool/apt-mirror/var/index-urls.$i | awk '{print $1}'`
-    CURRENT=`grep -E "saved|not retrieving|404 Not Found" /var/spool/apt-mirror/var/index-log.$i | wc -l`
-    echo "    $i - $CURRENT/$TOTAL"
-done
-
-echo "Archive status:"
-
-for i in `ls /var/spool/apt-mirror/var/archive-urls.* | cut -d. -f2`
-do
-    TOTAL=`wc -l /var/spool/apt-mirror/var/archive-urls.$i | awk '{print $1}'`
-    CURRENT=`grep saved /var/spool/apt-mirror/var/archive-log.$i | wc -l`
-    echo "    $i - $CURRENT/$TOTAL"
-done
+indexStatus
+archiveStatus
