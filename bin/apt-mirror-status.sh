2d70f42be7b6d843c32b8606e3cff1ba2677c22e
dougfultz <dougfultz@users.noreply.github.com>
2016-04-13 10:01:41 -0400

    add apt-mirror-status.sh

---
+++ bin/apt-mirror-status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Index status:"
+
+for i in `ls /var/spool/apt-mirror/var/index-urls.* | cut -d. -f2`
+do
+    TOTAL=`wc -l /var/spool/apt-mirror/var/index-urls.$i | awk '{print $1}'`
+    CURRENT=`grep -E "saved|not retrieving|404 Not Found" /var/spool/apt-mirror/var/index-log.$i | wc -l`
+    echo "    $i - $CURRENT/$TOTAL"
+done
+
+echo "Archive status:"
+
+for i in `ls /var/spool/apt-mirror/var/archive-urls.* | cut -d. -f2`
+do
+    TOTAL=`wc -l /var/spool/apt-mirror/var/archive-urls.$i | awk '{print $1}'`
+    CURRENT=`grep saved /var/spool/apt-mirror/var/archive-log.$i | wc -l`
+    echo "    $i - $CURRENT/$TOTAL"
+done
